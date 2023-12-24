2cbc51558f638e0121e49012400c1f78e842b2f9
bochr <bochra.isitcom@gmail.com>
2023-12-24 19:09:35 +0100

    Added Db

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineSchoolWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineSchoolWebApp.Models;
+using OnlineSchoolWebApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolDb")));
 
 var app = builder.Build();
 
