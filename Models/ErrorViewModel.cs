fbd950a9eb802ff5860674ef996961bb9ad1d0f6
bochr <bochra.isitcom@gmail.com>
2023-12-24 18:32:41 +0100

    my very first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineSchoolWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/js/SideBar.js
@@ -0,0 +1,12 @@
+﻿$(".sidebar ul li").on('click', function () {
+    $(".sidebar ul li.active").removeClass('active');
+    $(this).addClass('active');
+});
+
+$('.open-btn').on('click', function () {
+    $('#side_nav').addClass('active');
+});
+
+$('.close-btn').on('click', function () {
+    $('#side_nav').removeClass('active');
+});
